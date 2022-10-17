0791fc5fc348353f52fe3ef3a82601506119b5c3
LautaroBeutel <lautarob52@gmail.com>
2022-10-17 14:53:33 -0300

    l

--- Programa_menu/main.cpp
+++ Programa_menu/main.cpp
@@ -1,100 +1,70 @@
 #include <iostream>
 #include <ncurses.h>
+#include <string>
 using namespace std;
 
-void menu(WINDOW *body, int);
+typedef struct{
+	int a;
+	int b;
+}page_menu;
 
-const char* opciones[] = {"opcion 1", "opcion 2", "opcion 3", "opcion 4"};
+void menu(page_menu *main);
+void seleccion(page_menu *main);
+
+const char *opciones[] = {"opcion 1", "opcion 2", "opcion 3", "opcion 4"};
 
 int main(){
+	
+	page_menu main_menu = {0, 0};
+
 	initscr();
 
 	keypad(stdscr, TRUE);
 	curs_set(0);		
 	
-	int max_x, max_y, col, rowls;
-	int start_x, start_y;
-
-	int ch;
-       	int op = 0;
+	WINDOW *main = newwin(20, 20, 0, 0);
 	
-	col = 50;
-	rowls = 20;
+	seleccion(&main_menu);
 
-	getmaxyx(stdscr, max_y, max_x);
-
-	start_x = (max_x - col) / 2;
-	start_y = (max_y - rowls) / 2;
-
-	printw("press F1 to exit");
-
-	refresh();
-
-	WINDOW *main_body = newwin(rowls, col, start_y, start_x);
-	
-	box(main_body, 0, 0);
-	wrefresh(main_body);
-	
-	for(int i = 1; i <= 4; i++){
-		mvwprintw(main_body, i, 2,"%s", opciones[i-1]);
-	}
-	printw("\n");
-	wrefresh(main_body);
-	
-	while((ch = getch()) != KEY_F(1))
-	{	switch(ch)
-		{	
-			case KEY_UP:
-				op--;
-				op = op < 0 ? 0 : op;
-				break;
-			case KEY_DOWN:
-				op++;
-				op = op > 3 ? 3 : op;
-				break;	
-		}
-		menu(main_body, op);
-	}
-	
-	//getch();
-	
 	endwin();
 	return 0;
 }
 
-void menu(WINDOW *body, int op){
-	switch(op){
-		case 0:	wattron(body, A_STANDOUT);
-			mvwprintw(body, 1, 2, "%s", opciones[0]);
-			wattroff(body, A_STANDOUT);
-			mvwprintw(body, 2, 2, "%s", opciones[1]);
-			mvwprintw(body, 3, 2, "%s", opciones[2]);
-			mvwprintw(body, 4, 2, "%s", opciones[3]);
-			wrefresh(body);
+void seleccion(page_menu *ptrmain){
+	int ch;
+	while((ch = getch()) != KEY_F(1)){	
+		switch(ch){
+			case KEY_UP:	ptrmain->a--;
+					ptrmain->a = ptrmain->a < 0 ? 0 : ptrmain->a;
+				break;
+			case KEY_DOWN:	ptrmain->a++;
+					ptrmain->a = ptrmain->a > 4 ? 4 : ptrmain->a;
+				break;
+			case 'q':	ptrmain->b--;
+					ptrmain->b = ptrmain->b < 0 ? 0 : ptrmain->b;
+				break;
+			case 10:	ptrmain->b++;
+					ptrmain->b = ptrmain->b > 1 ? 1: ptrmain->b;
+				break;
+		}
+		menu(ptrmain);
+	}
+}
+
+void menu(page_menu *ptrmain){
+	switch(ptrmain->a){
+		case 0:	printw("\r%s", opciones[0]);
+			printw("\t%d", ptrmain->b);
 			break;
-		case 1:	wattron(body, A_STANDOUT);
-			mvwprintw(body, 2, 2, "%s", opciones[1]);
-			wattroff(body, A_STANDOUT);
-			mvwprintw(body, 1, 2, "%s", opciones[0]);
-			mvwprintw(body, 3, 2, "%s", opciones[2]);
-			mvwprintw(body, 4, 2, "%s", opciones[3]);
-			wrefresh(body);
+		case 1:	printw("\r%s", opciones[1]);
+			printw("\t%d", ptrmain->b);
 			break;
-		case 2:	wattron(body, A_STANDOUT);
-			mvwprintw(body, 3, 2, "%s", opciones[2]);
-			wattroff(body, A_STANDOUT),
-			mvwprintw(body, 1, 2, "%s", opciones[0]);
-			mvwprintw(body, 2, 2, "%s", opciones[1]);
-			mvwprintw(body, 4, 2, "%s", opciones[3]);
-			wrefresh(body);
+		case 2:	printw("\r%s", opciones[2]);
+			printw("\t%d", ptrmain->b);
 			break;
-		case 3:	wattron(body, A_STANDOUT);
-			mvwprintw(body, 4, 2, "%s", opciones[3]);
-			wattroff(body, A_STANDOUT);
-			mvwprintw(body, 1, 2, "%s", opciones[0]);
-			mvwprintw(body, 2, 2, "%s", opciones[1]);
-			mvwprintw(body, 3, 2, "%s", opciones[2]);
-			wrefresh(body);
+		case 3: printw("\r%s", opciones[3]);
+			printw("\t%d", ptrmain->b);
 			break;
 	}
+	refresh();
 }
